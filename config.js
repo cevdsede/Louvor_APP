50fb286ee62586069e48400c0b450f9cbdf8aa95
ewertonbsilva <ewerton.bezerra.silva@gmail.com>
2026-01-17 11:25:14 -0500

    troca de url

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const VERSION = '66';
 const APP_CONFIG = {
     VERSION,
     CACHE_NAME: `louvor-app-v${VERSION}`,
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwIZzKqrKGRrJQgKLjMHgWfxWxFQJDLgOSGQmz_Uj8Uy5Jw-hWUYPDPnXrVhCBZXOFu/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwjwn6-sdv8f4BLLwaqQWPc4yNI8CS40gO8J77GrJDqLncENJncWIfAV-FBkZuZP6k/exec"
 };
